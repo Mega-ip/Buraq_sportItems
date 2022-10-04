d383f22d42a70893e6fa347a1e4fbb01d546ee93
Ehab Melhem <Melhemehab@gmail.com>
2022-10-04 15:53:57 +0300

    setup responsive to header

--- app/src/App.js
+++ app/src/App.js
@@ -2,20 +2,22 @@ import "./App.css";
 import Header from "./MainView/Header/Header";
 import Leftbar from "./MainView/Leftbar/Leftbar";
 import Footer from "./MainView/Footer/Footer";
-
+import CardItem from "./Genral/CardItem/CardItem";
 function App() {
   return (
     <div className="app">
       {/* header */}
       <Header />
       {/* container */}
+      <CardItem />
       {/* left bar */}
       <Leftbar />
       {/* small footer */}
-      <Footer/>
-
+      <Footer />
     </div>
   );
 }
+
+// I have to make the header and the left bar is responsive
 
 export default App;
--- app/src/redux/reducers/main.js
+++ app/src/redux/reducers/main.js
@@ -1,20 +1,27 @@
 import * as types from "../ActionTpes";
+import ContentPaste from "@mui/icons-material/ContentPaste";
+import Divider from "@mui/material/Divider";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
 const initialState = {
   headerItems: [
     {
       label: "Home",
       value: "Home",
       active: true,
+      Icon: HomeIcon,
     },
     {
       label: "About us",
       value: "About_US",
       active: false,
+      Icon: InfoIcon,
     },
     {
       label: "Contact Us",
       value: "Contact_Us",
       active: false,
+      Icon: ContentPaste,
     },
   ],
 };
